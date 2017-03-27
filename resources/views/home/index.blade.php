20f1134f1349b1cd68ceb7fb10acddc2099a2acb
Jeroen <jeroenvenderbosch@hotmail.com>
2017-03-27 20:50:54 +0100

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+
+<html lang="nl-NL">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Pixelwolf</title>
+	<link rel="icon" href="{!! url('favicon.ico') !!}" type="image/ico" />
+	<link rel="shortcut icon" href="{!! url('favicon.ico') !!}" type="image/ico" />
+	<link rel="stylesheet" href="{!! url('css/bootstrap.min.css') !!}">
+	<link rel="stylesheet" href="{!! url('css/bootstrap-theme.min.css') !!}">
+	<link rel="stylesheet" href="{!! url('css/reset.css') !!}">
+	<link rel="stylesheet" href="{!! url('css/main.css') !!}">
+</head>
+<body>
+
+	<header id="header">
+		<div class="intro-header">
+			<div class="col-md-4">
+				<div class="logo">
+					<img src="{!! url('images/pixelwolf_logo_icon_white.png') !!}">
+				</div>
+			</div>
+			<div class="col-md-8">
+				<nav>
+					<ul class="nav nav-pixel">
+						<li class="nav-home"><a href="#">Home</a></li>
+						<li class="nav-about"><a href="#">About</a></li>
+						<li class="nav-visuals"><a href="#">Visuals</a></li>
+					</ul>
+					<button class="btn btn-work" href="#">Samen werken</button>
+				</nav>
+			</div>
+		</div>
+		<div class="floater-header">
+			<div class="floater-flex">
+				<div class="col-md-4">
+					<div class="logo">
+						<img src="{!! url('images/pixelwolf_logo_icon_white.png') !!}">
+					</div>
+				</div>
+				<div class="col-md-8">
+					<nav>
+						<ul class="nav nav-pixel">
+							<li class="nav-home"><a href="#">Home</a><span class="line"></span></li>
+							<li class="nav-about"><a href="#">About</a><span class="line"></span></li>
+							<li class="nav-visuals"><a href="#">Visuals</a><span class="line"></span></li>
+						</ul>
+						<button class="btn btn-work" href="#">Samen werken</button>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section id="intro">
+		<div class="intro overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="sub-title">Tim Helthuis</h2>
+					<h1>Professional Video Solutions</h1>
+					<button class="btn btn-work">Samen werken</button>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="home">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>The feeling you get when something visually moves you in a way you didn't know possible.</h2>
+					<h4>It's what I strive for in everything I photograph, film & produce. </h4>
+				</div>
+				<div class="clearfix"></div>
+				<div class="home">
+					<div class="col-md-4">
+						<img src="{!! url('/images/bg/Style.jpg') !!}" alt="Style">
+						<h3>Style</h3>
+						<p>Geen sets, geen gedoe. De “run en gun” style waarmee ik Werk zorgt voor een betrokken, creatieve en effectieve stijl waarin tijd en ruimte is voor elke situatie.</p>
+					</div>
+					<div class="col-md-4">
+						<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						<h3>International</h3>
+						<p>Of je nou retreats organiseerd op Ibiza, workshops geeft op Cuba of een klein café runt in het noorden van Scandinavie. Ik ga waar het verhaal verteld moet worden.</p>
+					</div>
+					<div class="col-md-4">
+						<img src="{!! url('/images/bg/Collaboration.jpg') !!}" alt="Collaboration">
+						<h3>Collaboration</h3>
+						<p>Samenwerken of volledig uitbesteden. Ik sta open voor elke vorm van samenwerking.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="about">
+		<div class="about overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Tim Helthuis the man,<br>
+					<span class="semi-bold">Pixelwolf the company</span></h2>
+					<h4>Een “run en gun” videograaf gespecialiseerd in Visual Vibes/Sfeer Impressies.<br/>Ik maak gebruik van hoge kwaliteit camera aparatuur en voornamelijk natuurlijk licht en omgeving.<br/>Geen grote productie sets zodat ik de vibe en spirit in zijn puurste en meest inspirationele vorm kan gebruiken.<br/>Werken met klant en omgeving is zo heel vrij.
+					</h4>
+					<h3>Reizen?<br/>Geen probleem.<br/><br/>Persoonlijk contact?<br/>Geen probleem.<br/><br/>Perfectie?<br/>Standaard.</h3>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="visuals">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2>"Stills from other projects"</h2>
+				</div>
+			</div>
+			<div class="row" style="margin:15px 0;">
+				<div class="col-md-6">
+					<div class="row" style="padding:0 0 15px;">
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/Style.jpg') !!}" alt="International">
+						</div>
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						</div>
+					</div>
+					<div class="row" style="padding:15px 0 0;">
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						</div>
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/Style.jpg') !!}" alt="International">
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+				</div>
+			</div>
+			<div class="row" style="padding:15px ;">
+				<div class="col-md-4">
+					<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+				</div>
+				<div class="col-md-4">
+					<img src="{!! url('/images/bg/Style.jpg') !!}" alt="International">
+				</div>
+				<div class="col-md-4">
+					<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+				</div>
+			</div>
+			<div class="row" style="margin:15px 0;">
+				<div class="col-md-6">
+					<img src="{!! url('/images/bg/Style.jpg') !!}" alt="International">
+				</div>
+				<div class="col-md-6">
+					<div class="row" style="padding:0 0 15px">
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						</div>
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						</div>
+					</div>
+					<div class="row" style="padding: 15px 0 0">
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/International.jpg') !!}" alt="International">
+						</div>
+						<div class="col-md-6">
+							<img src="{!! url('/images/bg/Style.jpg') !!}" alt="International">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="videography">
+		<div class="videography-content">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>Videography</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="video-overlay" style="position:relative;">
+			<div class="overlay"></div>
+			<video autoplay loop muted>
+				<source src="https://player.vimeo.com/external/210136156.hd.mp4?s=cdb56b958509f4129b41f187503feb7341b39325&profile_id=119" type="video/mp4">
+			</video>
+		</div>
+	</section>
+	<section id="samen-werken">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	<footer id="footer" class="home">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<script type="text/javascript" src="js/jquery-3.2.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery.inview.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/main.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('#about h4').on('inview',function(event, isInView){
+				$('.floater-header').addClass('toggled');
+				console.log(event);
+				console.log(isInView);
+			});
+			$('#intro').on('inview', function(event, isInView){
+				console.log(event);
+				console.log(isInView);
+				$('.floater-header').removeClass('toggled');
+			});
+		});
+	</script>
+</body>
+</html>
